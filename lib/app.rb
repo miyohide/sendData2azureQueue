5849c1ca9fd8cf97e87149418fe57f9a46c1d7c8
Hidenori Miyoshi <miyohide@gmail.com>
2021-05-26 20:21:27 +0900

    define constant

--- lib/app.rb
+++ lib/app.rb
@@ -3,11 +3,12 @@ require 'azure/storage/queue'
 # client = Azure::Storage::Queue::QueueService.create(
 #   use_development_storage: true
 # )
+STORAGE_ACCOUNT_NAME = "a1"
 
 client = Azure::Storage::Queue::QueueService.create(
-  storage_account_name: "a1",
+  storage_account_name: STORAGE_ACCOUNT_NAME,
   storage_access_key: "k1",
-  storage_queue_host: "http://127.0.0.1:10001/a1"
+  storage_queue_host: "http://127.0.0.1:10001/#{STORAGE_ACCOUNT_NAME}"
 )
 
 QUEUE_NAME = "myqueue"
