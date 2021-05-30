2018bb4409194e385e8fe4096520c1ff54fe0c9c
Hidenori Miyoshi <miyohide@gmail.com>
2021-05-30 17:30:32 +0900

    move ruby directory

--- ruby/lib/app.rb
+++ ruby/lib/app.rb
@@ -4,18 +4,18 @@ require 'base64'
 # client = Azure::Storage::Queue::QueueService.create(
 #   use_development_storage: true
 # )
-STORAGE_ACCOUNT_NAME = "a1"
+STORAGE_ACCOUNT_NAME = "miyohidequeue"
 
 client = Azure::Storage::Queue::QueueService.create(
   storage_account_name: STORAGE_ACCOUNT_NAME,
-  storage_access_key: "k1",
-  storage_queue_host: "http://127.0.0.1:10001/#{STORAGE_ACCOUNT_NAME}"
+  storage_access_key: "アクセスキー",
+  storage_queue_host: "https://#{STORAGE_ACCOUNT_NAME}.queue.core.windows.net/"
 )
 
 QUEUE_NAME = "myqueue"
 
 client.create_queue(QUEUE_NAME)
 
-30.times do |index|
+20.times do |index|
   client.create_message(QUEUE_NAME, Base64.encode64("test message #{index}"))
 end
